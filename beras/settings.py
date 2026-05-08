ae3d13f29cc5eb98a95acfa3f00de9f1478e903f
servatiusadhii <servatiusadhi13@gmail.com>
2026-05-08 10:54:33 +0700

    adding test sql

--- beras/settings.py
+++ beras/settings.py
@@ -88,15 +88,11 @@ TEMPLATES = [
 # DATABASE CONFIGURATION
 # =============================
 # Deteksi otomatis: Pakai MySQL Railway jika ada, kalau tidak pakai MariaDB XAMPP lokal
-load_dotenv()
-
-DATABASE_URL = os.getenv("MYSQL_URL")
-
 DATABASES = {
-    'default': dj_database_url.parse(
-        DATABASE_URL,
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # =============================
